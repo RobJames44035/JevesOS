4adca5021d833c7fb8606849ceb6a9b5e36f0180
rajames <RobJames44035@gmail.com>
2024-11-08 17:53:41 -0500

    Remove stage_two.asm and integrate its functionality
    The second stage loader code from stage_two.asm has been directly embedded into starship.asm for better maintainability. This change also simplifies the build process by removing the need to compile stage_two.asm separately in the pom.xml configuration.

--- src/main/asm/starship.asm
+++ src/main/asm/starship.asm
@@ -1,26 +1,54 @@
 [BITS 16]
 [ORG 0x7C00]
 
+; First Stage Bootloader Code
 start:
-    ; Load second stage loader
-    mov ax, 0x1000
+    ; Clear screen (optional for visualization)
+    mov ax, 0x0600
+    mov bh, 0x07
+    mov cx, 0x0000
+    mov dx, 0x184F
+    int 0x10
+
+    ; Load second stage loader into memory at 0x9000:0000
+    mov ax, 0x9000    ; Set segment for second stage
     mov es, ax
-    mov bx, 0x0000
-    add ax, 0x0010
+    mov si, boot_code
 
-    ; Load 512 bytes from disk to 0x1000:0000 (second stage)
-    mov ah, 0x02
-    mov al, 0x01
-    mov ch, 0x00
-    mov cl, 0x02
-    mov dh, 0x00
-    int 0x13
+    ; Copy second stage loader to 0x9000:0000
+copy_stage_two:
+    lodsb
+    stosb
+    cmp si, (boot_code + stage_two_size)
+    jne copy_stage_two
 
-    ; Jump to second stage loader
-    jmp 0x1000:0000
+    ; Jump to second stage loader at 0x9000:0000
+    jmp 0x9000:0000
 
 hang:
-    jmp hang
+    ; Simple hang for debug
+    jmp $
 
-times 510-($-$$) db 0
-dw 0xAA55
+boot_code:
+    ; Second Stage Loader Code Embedded Here
+    times 512 - ($ - $$) db 0
+stage_two_start:
+    ; Print welcome message
+    mov si, welcome_msg
+print_msg:
+    lodsb
+    cmp al, 0
+    je hang_stage_two
+    mov ah, 0x0E
+    int 0x10
+    jmp print_msg
+
+hang_stage_two:
+    ; Hang if message prints successfully
+    jmp $
+
+welcome_msg db 'Welcome to Stage Two', 0
+stage_two_size equ ($ - stage_two_start)
+times 1024 - ($ - $$) db 0 ; Filling to ensure that it fits within an appropriate number of sectors
+
+dw 0xAA55 ; Boot signature to mark the end of the bootloader
